893c666fcbaace4eec52fb651ddaa8d2cd78250e
Alex Ogier <alex.ogier@gmail.com>
2012-03-28 10:50:30 -0400

    First try at the actual implementation

---
+++ signoffs.js
@@ -0,0 +1,25 @@
+var randomize_signoff = function () {
+    var signoffs = [
+        "Ciao",
+        "Cheers",
+        "Regards"
+    ];
+
+    var el = document.activeElement;
+
+    var val = el.value;
+    var start = el.selectionStart;
+    var end = el.selectionEnd;
+
+    var prefix = val.substring(0, start);
+    var suffix = val.substring(end);
+
+    var tmpl = document.getElementById('__signoffs_js').getAttribute('data-template');
+    var signoff = signoffs[Math.floor(Math.random() * signoffs.length)];
+    var signature = tmpl.replace('{{}}', signoff);
+
+    el.value = prefix + signature + suffix;
+    el.setSelectionRange(start, start + signature.length);
+};
+
+randomize_signoff();
